f3c346b6eea852c45105a096a9efeacc6f29509b
EfrainRP <126370715+EfrainRP@users.noreply.github.com>
2024-01-03 18:19:10 -0600

    Add files via upload

---
+++ Tarea/tarea/tarea/main.asm
@@ -0,0 +1,21 @@
+;
+; tarea.asm
+;
+; Created: 16/11/2021 00:22:45
+; Author : efrain
+;
+.CSEG ; MEMORIA FLASH
+.ORG 0 ;comienza en la 0
+
+INICIA:
+    LDI R16, 0XFF ; cargar el valor 0XFF en el registro 16 (todos en valor de 1 en los 8 bits)
+	OUT DDRB, R16 ; configura el puerto B de acuerdo al valor del registro 16 anterior, como salida
+
+	LDI R16, 0B00000001 ; vuelve a cargar el valor 1 en binario en el registro 16
+	OUT PORTB, R16 ; saca en el puerto B el valor del registro 16 anterior(1 en binario)
+
+	LDI R16, 0B00000000 ; vuelve a cargar el valor 0 en binario en el registro 16
+	OUT PORTB, R16 ; saca en el puerto B el valor del registro 16 anterior(0 en binario)
+
+RJMP INICIA ; hara un salto a la etiqueta INICIA, para hacer las instrucciones dentro de ella 
+
---
+++ practica 8/CONTADOR/CONTADOR/main.asm
@@ -0,0 +1,43 @@
+;
+; CONTADOR.asm
+;
+; Created: 17/11/2021 19:46:40
+; Author : ING efrain
+;
+
+
+.CSEG
+.ORG 0
+
+LDI R16, 0XFF
+OUT DDRB, R16
+
+;CALL realiza una llamada 
+
+
+INICIA:
+	LDI R16, 0X00
+	OUT PORTB, R16
+	CALL TEMP
+
+CICLO:
+	INC R16
+	OUT PORTB, R16
+	CALL TEMP
+	CPI R16,32
+	BREQ INICIA
+
+RJMP CICLO
+
+TEMP:
+		LDI R20, 250
+TEMP2: 
+		NOP;es una instruccion sin operacion
+		NOP
+		NOP
+		NOP
+		DEC R20 ;destino(decrementa)
+		BRNE TEMP2
+		DEC R19 ;destino(decrementa)
+		BRNE TEMP; realiza un desvio condicional relativo 
+		RET ;se detiene
